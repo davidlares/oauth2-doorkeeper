274efb13176b7cd9156e482ede23dde237aee87e
David E Lares S <david.e.lares@gmail.com>
2019-01-23 12:46:23 -0600

    BCrypt GEM installed, Before Create crypt method on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  # requiring library
+  require "bcrypt"
+
+  # model exec
+  before_create :set_encrypt_password
+
+  #validations
+  validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+
+  # encrypting password
+  def set_encrypt_password
+    self.password = BCrypt::Password.create self.password
+  end
+
 end
